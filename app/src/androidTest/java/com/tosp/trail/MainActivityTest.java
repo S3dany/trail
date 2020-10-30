ba53ef7bd9f38d82e34131b0007f4dbc4fcf01a1
S3dany <s3dany88@gmail.com>
2020-10-30 07:11:32 +0100

    testing setup

---
+++ app/src/androidTest/java/com/tosp/trail/MainActivityTest.java
@@ -0,0 +1,21 @@
+package com.tosp.trail;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onCreateOptionsMenu() {
+    }
+
+    @Test
+    public void onOptionsItemSelected() {
+        assertNotNull(null);
+    }
+}
