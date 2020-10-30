6b47d9de7391ca7a0cae93c0d4dff21a8eb4a4a0
S3dany <s3dany88@gmail.com>
2020-10-30 07:04:09 +0100

    Initial commit

---
+++ app/src/androidTest/java/com/tosp/trail/InstrumentedTestSuite.java
@@ -0,0 +1,12 @@
+package com.tosp.trail;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+// Runs all unit tests.
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ExampleInstrumentedTest.class
+
+})
+public class InstrumentedTestSuite {}
